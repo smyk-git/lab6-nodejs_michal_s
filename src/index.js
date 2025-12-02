e437212c331c29936832eea241d88edd736b8858
Michał Smykowski <michal@test.com>
2025-12-02 18:53:59 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Docker + GitHub Actions!')
+  res.end('Hello from Docker + GitHub Actions! test')
 })
 
 server.listen(3000)
